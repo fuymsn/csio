e5ad6af46ef746b2a570cbc0f76b1f9787aecff8
Young Foo <fuymsn@hotmail.com>
2015-11-26 16:11:57 +0800

    Update umd-simple.js

--- umd-simple.js
+++ umd-simple.js
@@ -5,10 +5,16 @@
  
 !(function (name, factory) {
     if (typeof define === "function" && define.amd) {
+     
+        // AMD, Webpack
         define([], factory);
     } else if (typeof module === "object" && module.exports) {
+     
+        // Node, CommonJS-like
         module.exports = factory();
     } else {
+     
+        // Browser globals (this is window)
         this[name] = factory();
     }
 })('dialog', function () {
